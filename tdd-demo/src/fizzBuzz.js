925e4d22b5f2e502990200db952526b1bf4c75c0
BozenaMatak <57918562+BozenaMatak@users.noreply.github.com>
2020-04-15 15:46:59 +0200

    Delete fizzBuzz.js

--- tdd-demo/src/fizzBuzz.js
+++
@@ -1,8 +0,0 @@
-function fizzBuzz(value){
-    let result = [];
-    if (value % 3 === 0) result.push('fizz');
-    if (value % 5 === 0) result.push('Buzz');   
-    if (!result.length) result.push(value);
-    return result;    
-}
-module.exports = fizzBuzz;
